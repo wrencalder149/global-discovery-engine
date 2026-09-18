a1feaaecaced0e7ed9ea32df63749568e1e4808d
wrencalder149 <wrencalder.vq@gmail.com>
2026-09-18 10:22:47 +0800

    Collect film, music, history and literature sources every day

---
+++ src/culture-sources.js
@@ -0,0 +1,21 @@
+export const CULTURE_RSS_SOURCES = [
+  { name: "Variety", base_url: "https://variety.com/", feed_url: "https://variety.com/feed/", language: "en", country: "US", region: "global", source_type: "culture" },
+  { name: "IndieWire", base_url: "https://www.indiewire.com/", feed_url: "https://www.indiewire.com/feed/", language: "en", country: "US", region: "global", source_type: "culture" },
+  { name: "Pitchfork", base_url: "https://pitchfork.com/", feed_url: "https://pitchfork.com/feed/feed-news/rss", language: "en", country: "US", region: "global", source_type: "culture" },
+  { name: "NPR Music", base_url: "https://www.npr.org/music/", feed_url: "https://feeds.npr.org/1039/rss.xml", language: "en", country: "US", region: "global", source_type: "culture" },
+  { name: "The Quietus", base_url: "https://thequietus.com/", feed_url: "https://thequietus.com/feed/", language: "en", country: "GB", region: "Europe", source_type: "culture" },
+  { name: "Literary Hub", base_url: "https://lithub.com/", feed_url: "https://lithub.com/feed/", language: "en", country: "US", region: "global", source_type: "culture" },
+  { name: "Paris Review", base_url: "https://www.theparisreview.org/", feed_url: "https://www.theparisreview.org/feed/", language: "en", country: "US", region: "global", source_type: "culture" },
+  { name: "London Review of Books", base_url: "https://www.lrb.co.uk/", feed_url: "https://www.lrb.co.uk/feeds/rss", language: "en", country: "GB", region: "Europe", source_type: "culture" },
+  { name: "Aeon", base_url: "https://aeon.co/", feed_url: "https://aeon.co/feed.rss", language: "en", country: "AU", region: "global", source_type: "culture" },
+  { name: "JSTOR Daily", base_url: "https://daily.jstor.org/", feed_url: "https://daily.jstor.org/feed/", language: "en", country: "US", region: "global", source_type: "culture" },
+  { name: "History Today", base_url: "https://www.historytoday.com/", feed_url: "https://www.historytoday.com/feed/rss.xml", language: "en", country: "GB", region: "Europe", source_type: "culture" },
+  { name: "Hyperallergic Culture", base_url: "https://hyperallergic.com/", feed_url: "https://hyperallergic.com/feed/", language: "en", country: "US", region: "global", source_type: "culture" }
+];
+
+export const CULTURE_GOOGLE_RADARS = [
+  { name: "Google News Radar - film", query: "film festival OR cinema OR movie director OR screenplay" },
+  { name: "Google News Radar - music", query: "album OR composer OR concert OR opera OR jazz" },
+  { name: "Google News Radar - history", query: "history OR archaeology OR archive OR ancient OR heritage site" },
+  { name: "Google News Radar - literature", query: "novel OR poetry OR book review OR author OR literature" }
+];
